07de6e09abc8cee8efe50704eb5b480eab0463f5
Dave Letorey <dave@code-red.uk>
2021-10-19 15:52:11 +0100

    created the initialState for the app

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,111 @@ import DispatchContext from '../states/DispatchContext'
 
 function OnboardingApp({ Component, pageProps }) {
   const initialState = {
-    "name": "Simon",
+    flashMessages: [],
     app: {
+      loggedIn: false,
       loading: true,
-      firmData: false
+      urlDataFetched: true,
+      urlData: {},
+      localDataFetched: true,
+      localData: {},
+      firmData: false,
+      quoteData: false,
+      userData: false,
+      usersData: false,
+      firmUpdated: false,
+      quoteUpdated: false,
+      userUpdated: false,
+      usersUpdated: false,
+      indexOfAssociatedSolicitor: null,
+      indexOfLoggedInUser: null,
+      indexOfPrimaryUser: null,
+      AML: {
+        credas: {
+          enhancedAMLCode: "",
+          regTypes: []
+        },
+        thirdfort: {
+          jwt: "",
+          jwtExpiry: "",
+          tenant: {},
+          stubUser: {}
+        }
+      }
+    },
+    firm: {
+      id: "",
+      name: "",
+      url: "",
+      Colours: {
+        primary: "",
+        primaryOpposite: "",
+        secondary: "",
+        secondaryOpposite: "",
+        tertiary: "",
+        tertiaryOpposite: ""
+      },
+      logos: {
+        whiteSvg: "",
+        whitePng: "",
+        colourSvg: "",
+        colourPng: ""
+      },
+      solicitors: [],
+      modalContent: {}
+    },
+    quote: {
+      id: "",
+      associatedUsers: [],
+      associatedFirmId: "",
+      associatedSolicitorId: "",
+      AML: {
+        Provider: "",
+        thirdfort: {},
+        credas: {}
+      },
+      amlModalContent: "",
+      type: "",
+      addresses: []
+    },
+    users: [],
+    user: {
+      token: "",
+      id: "",
+      title: "",
+      otherTitle: "",
+      firstName: "",
+      surname: "",
+      email: "",
+      telephone: "",
+      telephoneNoDialCode: "",
+      dialCode: "",
+      contact: {
+        email: false,
+        tel: true,
+        primary: false
+      },
+      isPrimary: false,
+      agreed: {
+        gdpr: false,
+        tAndC: false,
+        all: false
+      },
+      passwordFailCount: 0,
+      passwordResetRequest: 0,
+      AML: {
+        credas: {
+          Sent: false,
+          Date: "",
+          Id: ""
+        },
+        thirdfort: {
+          Sent: false,
+          Date: "",
+          Id: ""
+        }
+      },
+      validated: false
     }
   }
   function ourReducer(draft, action) {
