c60d6b851d592210af0e9d79c9c963a1ca02690b
Dave Letorey <dave@code-red.uk>
2021-10-19 10:42:18 +0100

    added the Contexts to the _app and passed them down to index to use them

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
+// styles
 import '../styles/main.css'
 
+// packages
+import { useImmerReducer } from 'use-immer'
+
+//components
+
+// states
+import StateContext from '../states/StateContext'
+import DispatchContext from '../states/DispatchContext'
+
 function OnboardingApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const initialState = {
+    "name": "Simon"
+  }
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "one":
+        console.log("One")
+        console.log(action.value)
+        draft.name = action.value
+        return
+    }
+  }
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <Component {...pageProps} />
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  )
 }
 
 export default OnboardingApp
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useContext } from 'react'
 
 // components
 import Page from '../components/Main'
 
+// states
+import StateContext from '../states/StateContext'
+import DispatchContext from '../states/DispatchContext'
+
 export default function Home() {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+  function handleClick(e){
+    e.preventDefault()
+    appDispatch({type: "one", value: "Dave"})
+  }
   return (
     <Page title="Home">
-      <p>Dave was here</p>
+      <p>{appState.name} was here</p>
+      <p><a href="#" onClick={handleClick}>Change Name</a></p>
     </Page>
   )
 }
