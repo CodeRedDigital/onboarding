7218a62f896573d117b1b7d3d23e252b8ec5e41c
Dave Letorey <dave@code-red.uk>
2021-10-26 12:38:14 +0100

    created the ModalButton and ModalLink components

---
+++ components/ModalButton.js
@@ -0,0 +1,40 @@
+function ModalButton(props) {
+  return (
+    <>
+      <button
+        id={props.name + "Button"}
+        className="btn primary check"
+        data-modal-open={props.name + "Modal"}
+        data-button={props.name + "Button"}
+        data-checked={props.checked}
+        data-name={props.name}
+      >
+        {props.label}
+      </button>
+      <section className="modal-box" id={props.name + "Modal"} data-modal hidden="true">
+        <div className="modal-body" data-modal-document>
+          <header>
+            <h2>{props.label}</h2>
+            <h3>{props.instructions}</h3>
+          </header>
+          <div className="modal-content" id={props.name + "ModalContent"}>
+            {props.modalContent}
+          </div>
+          <footer>
+            <button className="btn warning" data-modal-close-btn>Cancel</button>
+            <button
+              id={props.name + "AcceptButton"}
+              className="btn primary"
+              data-accept=""
+              disabled
+            >
+              Accept
+            </button>
+          </footer>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default ModalButton
---
+++ components/ModalLink.js
@@ -0,0 +1,46 @@
+function ModalLink(props) {
+  return (
+    <>
+      <a
+        className=""
+        href={"#" + props.name + "Modal"}
+        data-modal-open={props.name + "Modal"}
+        data-button={props.name + "Button"}
+        data-name={props.name}
+        role="button"
+        tabIndex="0"
+        id={props.name + "Modal__trigger-A"}
+      >
+        {props.name}
+      </a>
+      <section
+        className="modal-box a11y-modal"
+        id={props.name + "Modal"}
+        data-modal=""
+        role="dialog"
+        tabIndex="-1"
+        aria-labelledby={props.name + "Modal_heading"}
+        hidden={true}
+      >
+        <div className="modal-body" data-modal-document="" role="document">
+          <header>
+            <h2 className="a11y-modal__heading" id={props.name + "Modal_heading"}>
+              About {props.name}
+            </h2>
+          </header>
+          <div className="modal-content" id={props.name + "ModalContent"}>
+            {props.modalLinkContent}
+          </div>
+          <footer>
+            <button className="btn warning js-first-focus" data-modal-close-btn="">
+              Close
+            </button>
+          </footer>
+          {/* <button type="button" className="a11y-modal__close-btn is-icon-btn js-last-focus" aria-label="Close"><span data-modal-x=""></span></button> */}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ModalLink
