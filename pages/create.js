543851fdc5252d5e803fc8928fb926377ef88385
Dave Letorey <dave@code-red.uk>
2021-10-27 12:42:34 +0100

    updated the create account page to have the fields

--- pages/create.js
+++ pages/create.js
@@ -1,24 +1,53 @@
 import { useContext } from "react"
+import Link from 'next/link'
 
 // contexts
 import StateContext from "../states/StateContext"
 import DispatchContext from "../states/DispatchContext"
 
 // components
-import Page from '../components/Main'
+import Main from '../components/Main'
 
 function Create() {
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
-  function handleClick(e){
-    e.preventDefault()
-    appDispatch({type: "one", value: "Dave"})
+  function handleSubmit(event){
+    event.preventDefault()
+    // send the details off to the backend
+    // create user
+    // get token on return
+    // flash message
   }
   return (
-    <Page title="Create">
-      <p>{appState.name} was here</p>
-      <p><a href="#" onClick={handleClick}>Change Name</a></p>
-    </Page>
+    <Main title="Create Account">
+      <h1>{appState.user.firstName} Create your Account</h1>
+      <p>Fill in the form below.</p>
+      <div className="column-row">
+        <form onSubmit={handleSubmit} className="form-grid">
+          <section className="form-grid">
+            <label htmlFor="email">email</label>
+            <div>
+              <input
+                onChange={e => setPassword(e.target.value)}
+                id="email"
+                type="email"
+                value={appState.user.email} // add the email into the value
+              />
+              <div className="alert alert-danger small"></div>
+            </div>
+            <label htmlFor="password">Password</label>
+            <div>
+              <input onChange={e => setPassword(e.target.value)} id="password" type="password" />
+              <div className="alert alert-danger small"></div>
+            </div>
+            <button className="btn primary" type="submit">
+              Submit
+            </button>
+          </section>
+        </form>
+      </div>
+      <p className="center">Already have an account <Link href="/login">Login</Link>.</p>
+    </Main>
   )
 }
 export default Create
