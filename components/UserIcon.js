a03b5f9d8d0711c46b00679a548e48e6930dac0d
Dave Letorey <dave@code-red.uk>
2021-10-28 17:32:11 +0100

    update user id for users from id to userId

---
+++ components/UserIcon.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+function SellerIcon(props) {
+  let current = ""
+  if (props.current == props.userId) {
+    current = "current"
+  }
+  return (
+    <Link className={current} to={`/users/${props.userId}`}>
+      <svg
+        width="30"
+        height="51"
+        viewBox="0 0 30 51"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M24.998 24.8L24.3118 25.5L24.998 26.2C27.4746 28.7263 29 32.184 29 36V50H1V36C1 32.184 2.5254 28.7263 5.00195 26.2L5.68822 25.5L5.00195 24.8C2.5254 22.2737 1 18.816 1 15C1 7.26801 7.26801 1 15 1C22.732 1 29 7.26801 29 15C29 18.816 27.4746 22.2737 24.998 24.8Z"
+          strokeWidth="2"
+        />
+      </svg>
+    </Link>
+  );
+}
+
+export default SellerIcon;
--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useContext } from 'react'
 
 // components
-import LoadingScreen from '../../components/LoadingScreen'
+import Main from '../../components/Main'
+import UserIcon from '../../components/UserIcon'
+import LoadingSpinner from '../../components/LoadingSpinner'
+import ModalLink from '../../components/ModalLink'
 
 // states
 import StateContext from '../../states/StateContext'
@@ -11,10 +12,11 @@ import DispatchContext from '../../states/DispatchContext'
 
 export default function Home() {
   const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
   return (
-    <>
+    <Main>
       <h1>Users</h1>
       <h2>Welcome {appState.app.userData === "success" ? appState.user.firstName : ""}</h2>
-    </>
+    </Main>
   )
 }
