4afc422184dd6fbbf863cd0a6288b07301676828
Dave Letorey <dave@code-red.uk>
2021-10-28 17:53:06 +0100

    created the beginning structure of the User page

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -1,4 +1,7 @@
 import { useContext } from 'react'
+import { useRouter } from 'next/router'
+import { useImmerReducer } from 'use-immer'
+import { AxiosPali } from '../../src/AxiosRequests'
 
 // components
 import Main from '../../components/Main'
@@ -11,12 +14,40 @@ import StateContext from '../../states/StateContext'
 import DispatchContext from '../../states/DispatchContext'
 
 export default function Home() {
-  const appState = useContext(StateContext);
-  const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+  const router = useRouter()
+  const {userId} = router.query
+  const initialCurrentUserState = {
+    userIsLoading: true,
+    userId: userId,
+    name: "Dave"
+  }
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "setName":
+        draft.name = `${action.firstName} ${action.surname}`;
+        return;
+      case "one":
+        console.log("This is the first case")
+        return
+    }
+  }
+  const [state, dispatch] = useImmerReducer(ourReducer, initialCurrentUserState)
+
+  // Page functions go here
+  // Page functions end here
+
   return (
     <Main>
-      <h1>Users</h1>
+      <h1>User { userId }</h1>
       <h2>Welcome {appState.app.userData === "success" ? appState.user.firstName : ""}</h2>
+      <ul>
+        <li>UserId: {state.userId}</li>
+        <li>Name: {state.name}</li>
+        <li></li>
+        <li></li>
+      </ul>
     </Main>
   )
 }
