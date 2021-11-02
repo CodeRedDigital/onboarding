ef1b2186bafddc7000e2684934425e96623318bc
Dave Letorey <dave@code-red.uk>
2021-11-02 15:55:58 +0000

    added the form and the buttons

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -3,8 +3,11 @@
 import { useEffect, useContext } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import Script from 'next/script'
 import { useImmerReducer } from 'use-immer'
 import { AxiosPali } from '../../src/AxiosRequests'
+import { decode } from "he" // used for decoding the encoded html for the modal
+import parse from 'html-react-parser'
 
 // components
 import Main from '../../components/Main'
@@ -16,15 +19,7 @@ import ModalLink from '../../components/ModalLink'
 import StateContext from '../../states/StateContext'
 import DispatchContext from '../../states/DispatchContext'
 
-// export async function getStaticPaths() {
-//   // Return a list of possible value for id
-// }
-
-// export async function getStaticProps({ params }) {
-//   // Fetch necessary data for the blog post using params.id
-// }
-
-export default function Home() {
+export default function User(props) {
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
   const router = useRouter()
@@ -133,16 +128,29 @@ export default function Home() {
   const [state, dispatch] = useImmerReducer(ourReducer, initialCurrentUserState)
 
   // Page functions go here
-  // function getAllUsers() {
-  //   const users = appState.quote.associatedUsers
-  //   return users.map(user => {
-  //     return {
-  //       params: {
-  //         id: user.id
-  //       }
-  //     }
-  //   })
-  // }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const buttonClicked = event.nativeEvent.submitter;
+    if (buttonClicked.getAttribute("data-send") === "all") {
+      sendAmlToUsers(appState.users);
+    } else {
+      const indexOfSingleUser = appState.users.findIndex(
+        user => user.id === id
+      );
+      sendAmlToUsers([appState.users[indexOfSingleUser]]);
+    }
+  }
+  async function getUser(userId) {
+    try {
+      const response = await AxiosPali.get(`/data/test/user/${userId}-user.json`);
+      if (response.data) {
+        appDispatch({ type: "pushUser", user: response.data }); // push the missing user into the users Array
+        dispatch({ type: "userDownloaded", user: response.data });
+      }
+    } catch (e) {
+      console.log("There was an issue getting the user");
+    }
+  }
   // Page functions end here
   // useEffects start here
   useEffect(() => {
@@ -203,13 +211,243 @@ export default function Home() {
         </div>
       </div>
       <h1>{ state.name }</h1>
-      <h2>Welcome {appState.app.userData === "success" ? appState.user.firstName : ""}</h2>
-      <ul>
-        <li>UserId: {state.userId}</li>
-        <li>Name: {state.name}</li>
-        <li>AML: {appState.quote.AML.Provider}</li>
-        <li>Primary: {(state.primary ? "Yes": "No")}</li>
-      </ul>
+      <div id="instructions">
+        <p>
+          We now need to check your details before we proceed. Please check
+          carefully that all the fields are filled in and correct.
+        </p>
+        <p>
+          Once all of these fields have been completed we will pass them over to
+          our trust partner{" "}
+            <ModalLink
+              name={appState.quote.AML.Provider}
+              href="#amlButton"
+              className=""
+              data-modal-open={appState.quote.AML.Provider + "Modal"}
+              data-button={appState.quote.AML.Provider + "Button"}
+              data-name={appState.quote.AML.Provider}
+              modalLinkContent={parse(unescape(state.thirdfort ? appState.firm.modalContent.thirdfort : appState.firm.modalContent.credas))}
+            />
+          , to do Identity and Financial checks.
+        </p>
+      </div>
+      <form onSubmit={handleSubmit} className="user">
+      <section className="form-grid">
+          <label htmlFor="title">Title</label>
+          <div>
+            <input
+              id="title"
+              type="text"
+              value={state.title.value}
+              disabled={state.isDisabled}
+              onChange={e =>
+                dispatch({ type: "titleUpdate", value: e.target.value })
+              }
+            />
+            <div
+              className="
+              alert alert-danger
+              small
+              liveValidateMessage liveValidateMessage-enter-done
+            "
+            >
+              Please enter your Title.
+            </div>
+          </div>
+          <label htmlFor="first-name">First name</label>
+          <div>
+            <input
+              id="first-name"
+              type="text"
+              value={state.firstName.value}
+              disabled={state.isDisabled}
+              onChange={e =>
+                dispatch({ type: "firstNameUpdate", value: e.target.value })
+              }
+            />
+            <div
+              className="
+              alert alert-danger
+              small
+              liveValidateMessage liveValidateMessage-enter-done
+            "
+            >
+              Please enter your first name.
+            </div>
+          </div>
+          <label htmlFor="surname">Surname</label>
+          <div>
+            <input
+              id="surname"
+              type="text"
+              value={state.surname.value}
+              disabled={state.isDisabled}
+              onChange={e =>
+                dispatch({ type: "surnameUpdate", value: e.target.value })
+              }
+            />
+            <div
+              className="
+              alert alert-danger
+              small
+              liveValidateMessage liveValidateMessage-enter-done
+            "
+            >
+              Please enter your Surname.
+            </div>
+          </div>
+          <label htmlFor="email">email address</label>
+          <div>
+            <input
+              id="email"
+              type="email"
+              value={state.email.value}
+              disabled={state.isDisabled}
+              onChange={e =>
+                dispatch({ type: "emailUpdate", value: e.target.value })
+              }
+            />
+            <div
+              className="
+              alert alert-danger
+              small
+              liveValidateMessage liveValidateMessage-enter-done
+            "
+            >
+              Please enter a valid email address.
+            </div>
+          </div>
+          <label htmlFor="telephone">Mobile telephone</label>
+          <div>
+            <input
+              id="telephone"
+              type="tel"
+              value={state.telephone.value}
+              disabled={state.isDisabled}
+              onChange={e =>
+                dispatch({ type: "telephoneUpdate", value: e.target.value })
+              }
+            />
+            <div
+              className="
+              alert alert-danger
+              small
+              liveValidateMessage liveValidateMessage-enter-done
+            "
+              data-input="telWarning"
+            >
+              Invalid phone number.
+            </div>
+          </div>
+          <label htmlFor="first-name" className="span-grid">
+              Send Identity Check
+            </label>
+            <div id="identity-hint" className="pali-hint">
+              Where would you like your identity check link sent to?
+            </div>
+            {!state.thirdfortAndPrimary && ( // check that the AML provider is thirdfort and the user is not Primary and then load radio buttons
+            <div className="pali-radios pali-radios">
+              {appState.users[appState.app.indexOfPrimaryUser].id !== state.id && (
+                <div className="pali-radios__item">
+                  <input
+                    className="pali-radios__input"
+                    id="send-to-primary"
+                    name="contact"
+                    type="radio"
+                    value="primary"
+                    checked={props.primarySelected}
+                    onClick={() =>
+                      dispatch({
+                        type: "contactUpdate",
+                        tel: false,
+                        email: false,
+                        primary: true
+                      })
+                    }
+                  />
+                  <label
+                    className="pali-label pali-radios__label"
+                    htmlFor="send-to-primary"
+                  >
+                    {appState.users[appState.app.indexOfPrimaryUser].firstName}'s phone
+                    {appState.quote.AML.Provider === "CREDAS" && "/email"}
+                  </label>
+                </div>
+              )}
+              {appState.quote.AML.Provider === "CREDAS" && (
+                <div className="pali-radios__item">
+                  <input
+                    className="pali-radios__input"
+                    id="send-to-email"
+                    name="contact"
+                    type="radio"
+                    value="email"
+                    disabled={state.isDisabled}
+                    checked={state.contact.email}
+                    onClick={() =>
+                      dispatch({
+                        type: "contactUpdate",
+                        tel: false,
+                        email: true,
+                        primary: false
+                      })
+                    }
+                  />
+                  <label
+                    className="pali-label pali-radios__label"
+                    htmlFor="send-to-email"
+                  >
+                    email
+                  </label>
+                </div>
+              )}
+              <div className="pali-radios__item">
+                <input
+                  className="pali-radios__input"
+                  id="send-to-tel"
+                  name="contact"
+                  type="radio"
+                  value="tel"
+                  disabled={state.isDisabled}
+                  checked={state.contact.tel}
+                  onClick={() =>
+                    dispatch({
+                      type: "contactUpdate",
+                      tel: true,
+                      email: false,
+                      primary: false
+                    })
+                  }
+                />
+                <label
+                  className="pali-label pali-radios__label"
+                  htmlFor="send-to-tel"
+                >
+                  telephone
+                </label>
+              </div>
+            </div>
+          )}
+        </section>
+        <button
+          className="btn primary"
+          disabled={state.isDisabled}
+          data-send={state.userId}
+        >
+          Send {appState.quote.AML.Provider} Link for {state.firstName.value}
+        </button>
+        {appState.user.id === state.userId &&
+          appState.users[appState.app.indexOfPrimaryUser].id === state.userId && (
+            <button
+              className="btn primary"
+              disabled={state.isDisabled}
+              data-send="all"
+            >
+              Send All {appState.quote.AML.Provider} Links
+            </button>
+          )}
+      </form>
+      <Script id="modal-js" src="/js/modal.js" onLoad={() => {ARIAmodal.init()}} />
     </Main>
   )
 }
