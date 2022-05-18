ada44fe34c2244387448cbb8f04dfdf5df0379af
Dave Letorey <dave@code-red.uk>
2022-05-18 12:52:01 +0100

    created a condition to check if sections and questions are available

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 
 // components
+import LoadingSpinner from "../../components/LoadingSpinner"
 import Main from "../../components/Main"
 
 // states
@@ -11,10 +12,20 @@ function QuestionsHome() {
   const appState = useContext(StateContext)
   const router = useRouter()
   const sectionName = router.query.section
-  return (
+  useEffect(() => {
+    if(appState.questions.length === 0) {
+      console.log("fetch questions")
+    }
+    if(appState.sections.length === 0) {
+      console.log("fetch sections")
+    }
+  })
+  return (appState.questions.length === 0 || appState.sections.length === 0 ? <Main title="loading"><LoadingSpinner /></Main> :
     <Main title="questions">
       <h1>Questions</h1>
       <p>Below are list of sections which contain questions that will help progress your {appState.quote.type}.</p>
+      <p>questions: {appState.questions.length}<br />
+      sections: {appState.sections.length}</p>
     </Main>
   )
 }
