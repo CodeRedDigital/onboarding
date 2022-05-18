9d23778b3aa4a8e83e72b49f7e8d00801b9da991
Dave Letorey <dave@code-red.uk>
2022-05-18 12:19:35 +0100

    added main to the questions home

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,9 +1,22 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
+import { useContext } from "react"
+
+// components
+import Main from "../../components/Main"
+
+// states
+import StateContext from "../../states/StateContext";
 
 function QuestionsHome() {
+  const appState = useContext(StateContext)
   const router = useRouter()
   const sectionName = router.query.section
-  return <h1>Questions Home</h1>
+  return (
+    <Main title="questions">
+      <h1>Questions</h1>
+      <p>Below are list of sections which contain questions that will help progress your {appState.quote.type}.</p>
+    </Main>
+  )
 }
 
 export default QuestionsHome
