6b786cc5a6424edd20cfae259c20102f652dc46e
Dave Letorey <dave@code-red.uk>
2022-06-07 17:09:26 +0100

    added the Link element

--- components/questions/section.js
+++ components/questions/section.js
@@ -1,19 +1,21 @@
+import Link from 'next/link'
+
 // components
 import Progress from "./progress"
 
 function Section(props) {
   const section = props.section
   const answered = Math.floor((Math.random() * section.questions.length) + 1)
-  console.log(section)
   return (
     <details>
       <summary>
         <h3>{section.sectionLabel} <span className="progress">{answered}/{section.questions.length}</span></h3>
       </summary>
       <Progress sectionId={section.sectionId} questions={section.questions.length} answered={answered}></Progress>
+      <Link href={`/questions/${section.sectionId}`}><a>View Section</a></Link>
       <ul>
         {section.questions.map((question, index) => {
-          return (<li key={index}><a href={`/questions/${section.sectionId}/${question.name}`}>{question.label} <span data-complete="true"></span></a></li>)
+          return (<li key={index}><Link href={`/questions/${section.sectionId}/${question.name}`}><a>{question.label} <span data-complete="true"></span></a></Link></li>)
         })}
       </ul>
     </details>
