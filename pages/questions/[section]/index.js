21c4d615d2b78aeba8c080acfead86995d3ba40c
Dave Letorey <dave@code-red.uk>
2022-06-07 17:16:49 +0100

    added the list of questions to the section

--- pages/questions/[section]/index.js
+++ pages/questions/[section]/index.js
@@ -1,15 +1,39 @@
+import { useContext, useEffect, useState } from "react"
 import { useRouter } from "next/router";
+import { Link } from "next/link"
 
 // components
+import LoadingSpinner from "../../../components/LoadingSpinner"
 import Main from "../../../components/Main";
+import QuestionLink from "../../../components/questions/QuestionLink"
+
+// states
+import StateContext from "../../../states/StateContext";
+
+// helpers
+import { getObjectIndexFromKey } from "../../../helpers/ArrayFunctions"
 
 function Section() {
   const router = useRouter()
-  const sectionName = router.query.section
-  return (
-    <Main>
-      <h1>This section is {sectionName}</h1>
-      <p>List of questions goes here</p>
+  const appState = useContext(StateContext)
+  const [section, setSection] = useState(null);
+  useEffect(() => {
+    if (router.isReady) {
+      const sectionId = router.query.section
+      const sections = appState.sortedQuestions
+      const indexOfSection = getObjectIndexFromKey(sections, "sectionId", sectionId)
+      setSection(sections[indexOfSection])
+    }
+  },[router.isReady])
+  // const answered = Math.floor((Math.random() * section.questions.length) + 1)
+  return (section === null ? <Main title="loading"><LoadingSpinner /></Main> :
+    <Main title={section.sectionLabel}>
+      <h1>{section.sectionLabel}</h1>
+      <ul>
+        {section.questions.map((question, index) => {
+          return (<QuestionLink key={index} section={section} question={question}></QuestionLink>)
+        })}
+      </ul>
     </Main>
   )
 }
