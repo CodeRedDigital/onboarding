33e5534f7d60115135139c5af4d3e9b35db6e77b
Dave Letorey <dave@code-red.uk>
2022-06-09 17:39:37 +0100

    created a Question Nav and loaded the data into the question

---
+++ components/questions/QuestionNav.js
@@ -0,0 +1,11 @@
+function QuestionNav() {
+  return (
+    <nav id="navigation">
+      <button class="btn secondary">back</button>
+      <button class="btn primary">home</button>
+      <button class="btn secondary">next</button>
+    </nav>
+  )
+}
+
+export default QuestionNav
--- pages/questions/[section]/[questionid].js
+++ pages/questions/[section]/[questionid].js
@@ -1,9 +1,34 @@
+import { useContext } from 'react'
 import { useRouter } from "next/router";
+
+// components
+import Main from '../../../components/Main'
+import QuestionNav from '../../../components/questions/QuestionNav.js'
+
+// states
+import StateContext from "../../../states/StateContext";
+import DispatchContext from "../../../states/DispatchContext";
+
+// helpers
+import { getObjectIndexFromKey } from '../../../helpers/ArrayFunctions'
 
 function Question() {
   const router = useRouter()
-  const questionName = router.query.question
-  return <h1>This question is {questionName}</h1>
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+  const questionName = router.query.questionid
+  const sectionId = router.query.section
+  const sortedQuestions = appState.sortedQuestions
+  const sectionIndex = getObjectIndexFromKey(sortedQuestions, "sectionId", sectionId)
+  const section = sortedQuestions[sectionIndex]
+  console.log(section)
+  return (
+    <Main title={questionName}>
+      <h1>This question is {questionName}</h1>
+      <h2>{sectionId}</h2>
+      <QuestionNav></QuestionNav>
+    </Main>
+  )
 }
 
 export default Question
