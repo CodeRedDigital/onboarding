ffea8e3843654ce1911e881f6a05f0ea58a8df5e
Dave Letorey <dave@code-red.uk>
2022-06-13 15:10:48 +0100

    Loaded the question into state

--- pages/questions/[section]/[questionid].js
+++ pages/questions/[section]/[questionid].js
@@ -1,7 +1,8 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useRouter } from "next/router";
 
 // components
+import LoadingSpinner from '../../../components/LoadingSpinner'
 import QuestionsMain from '../../../components/QuestionsMain'
 import QuestionNav from '../../../components/questions/QuestionNav.js'
 
@@ -16,17 +17,31 @@ function Question() {
   const router = useRouter()
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
+  const [loading,setLoading] = useState(false)
   const questionName = router.query.questionid
   const sectionId = router.query.section
   const sortedQuestions = appState.sortedQuestions
   const sectionIndex = getObjectIndexFromKey(sortedQuestions, "sectionId", sectionId)
   const section = sortedQuestions[sectionIndex]
-  console.log(section)
-  return (
+  let questionIndex = null
+  let question = null
+  useEffect(() => {
+    if (section) {
+      questionIndex = getObjectIndexFromKey(section.questions, "name", questionName)
+      console.log(questionIndex)
+    }
+    if (questionIndex) {
+      question = section.questions[questionIndex]
+      setLoading(true)
+      console.log(question)
+    }
+  },[section, questionIndex])
+  return (!loading ? <QuestionsMain title="Loading"><LoadingSpinner /></QuestionsMain> : 
     <QuestionsMain title={questionName} question="true">
       <div className="form-wrapper">
-        <h1>This question is {questionName}</h1>
+        <label>This question is {questionName}</label>
         <h2>{sectionId}</h2>
+        
       </div>
       <QuestionNav></QuestionNav>
     </QuestionsMain>
