20fbab3bbb6b9288daa88b6d0b01cb82ce46f34b
Dave Letorey <dave@code-red.uk>
2022-06-14 15:06:46 +0100

    created handleClick function to handle the router of the nav buttons

--- components/questions/QuestionNav.js
+++ components/questions/QuestionNav.js
@@ -1,9 +1,24 @@
-function QuestionNav() {
+import { useRouter } from 'next/router'
+
+function QuestionNav(props) {
+  const router = useRouter()
+  const prevPath = `/questions/${props.prev}`
+  const homePath = `/questions`
+  const nextPath = `/questions/${props.next}`
+  const handleClick = (event) => {
+    event.preventDefault()
+    console.log(event.target.dataset.path)
+    if(event.target.dataset.path){
+      console.log("Navigating")
+      router.push(event.target.dataset.path)
+    }
+  }
+  console.log(`Prev - ${props.prev} Next - ${props.next}`)
   return (
     <nav id="navigation">
-      <button className="btn secondary">back</button>
-      <button className="btn primary">home</button>
-      <button className="btn secondary">next</button>
+      <button onClick={handleClick} data-path={prevPath} className="btn secondary">back</button>
+      <button onClick={handleClick} data-path={homePath} className="btn primary">home</button>
+      <button onClick={handleClick} data-path={nextPath} className="btn secondary">next</button>
     </nav>
   )
 }
