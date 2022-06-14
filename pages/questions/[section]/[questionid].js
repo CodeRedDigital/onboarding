632ae0a1c183e6ffca96cf406042671b67135d41
Dave Letorey <dave@code-red.uk>
2022-06-14 12:50:47 +0100

    worked out the prev and next paths

--- pages/questions/[section]/[questionid].js
+++ pages/questions/[section]/[questionid].js
@@ -18,6 +18,8 @@ function Question() {
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
   const [loading,setLoading] = useState(false)
+  const [prev,setPrev] = useState()
+  const [next,setNext] = useState()
   const questionName = router.query.questionid
   const sectionId = router.query.section
   const sortedQuestions = appState.sortedQuestions
@@ -28,10 +30,40 @@ function Question() {
   useEffect(() => {
     if (section) {
       questionIndex = getObjectIndexFromKey(section.questions, "name", questionName)
-      console.log(questionIndex)
     }
     if (questionIndex) {
       question = section.questions[questionIndex]
+      // set prev & next question url
+      console.log(`section index = ${sectionIndex}`)
+      console.log(`question Index = ${questionIndex}`)
+      let sectionPath
+      let questionPath
+      if(sectionIndex === 0 && questionIndex === 0) {
+        setPrev(null)
+      } else {
+        if (questionIndex === 0) {
+          sectionPath = appState.sortedQuestions[sectionIndex -1].sectionId
+          questionPath = appState.sortedQuestions[sectionIndex -1].questions[appState.sortedQuestions[sectionIndex -1].questions.length -1].name
+        } else {
+          sectionPath = appState.sortedQuestions[sectionIndex].sectionId
+          questionPath = appState.sortedQuestions[sectionIndex].questions[questionIndex -1].name
+        }
+        setPrev(`${sectionPath}/${questionPath}`)
+        console.log(prev)
+      }
+      if(sectionIndex === (appState.sortedQuestions.length -1) && questionIndex === (appState.sortedQuestions[sectionIndex].questions.length - 1)) {
+        setNext(null)
+      } else {
+        if (questionIndex === appState.sortedQuestions[sectionIndex].questions.length - 1) {
+          sectionPath = appState.sortedQuestions[sectionIndex + 1].sectionId
+          questionPath = appState.sortedQuestions[sectionIndex + 1].questions[0].name
+        } else {
+          sectionPath = appState.sortedQuestions[sectionIndex].sectionId
+          questionPath = appState.sortedQuestions[sectionIndex].questions[questionIndex + 1].name
+        }
+        setNext(`${sectionPath}/${questionPath}`)
+        console.log(next)
+      }
       setLoading(true)
       console.log(question)
     }
@@ -43,7 +75,7 @@ function Question() {
         <h2>{sectionId}</h2>
         
       </div>
-      <QuestionNav></QuestionNav>
+      <QuestionNav prev={prev} next={next} ></QuestionNav>
     </QuestionsMain>
   )
 }
